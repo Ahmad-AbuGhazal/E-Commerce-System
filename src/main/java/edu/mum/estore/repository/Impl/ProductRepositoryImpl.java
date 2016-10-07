cbeaf1cafd5f01f29e1adfd5f8a8dc5b8ada14d0
Jannatul Ferdous Mou <moujannat786@gmail.com>
2016-10-07 17:42:28 -0500

    RepositoryImpl

--- src/main/java/edu/mum/estore/repository/Impl/ProductRepositoryImpl.java
+++ src/main/java/edu/mum/estore/repository/Impl/ProductRepositoryImpl.java
@@ -1,11 +1,13 @@
 package edu.mum.estore.repository.Impl;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
 import edu.mum.estore.domain.Product;
 import edu.mum.estore.repository.ProductRepository;
 
 @Repository
+@RestController
 public class ProductRepositoryImpl implements ProductRepository{
 
 	@Override
