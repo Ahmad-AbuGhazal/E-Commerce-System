333227bc214598f6f1390a17288ae162307972bf
Ahmad Abu Ghazal <ahmad_abughazal@outlook.com>
2016-10-12 14:51:43 -0500

    update ResponseInfo

---
+++ src/main/java/edu/mum/estore/domain/ResponseInfo.java
@@ -0,0 +1,19 @@
+package edu.mum.estore.domain;
+
+/**
+ * Class for @ResponseBody
+ * @author Lin
+ *
+ */
+
+public class ResponseInfo {
+	private char response;
+
+	public char getResponse() {
+		return response;
+	}
+
+	public void setResponse(char response) {
+		this.response = response;
+	}
+}
