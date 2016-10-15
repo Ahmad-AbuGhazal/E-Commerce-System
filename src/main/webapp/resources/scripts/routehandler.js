9312b1f26e0cbd56b7a90b6972068e2d2d030191
tek <tek.b.ale@gmail.com>
2016-10-15 14:26:35 -0500

    r

---
+++ src/main/webapp/resources/scripts/routehandler.js
@@ -0,0 +1,37 @@
+var app = angular.module('estore', ['ngRoute', 'rapp', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {
+            templateUrl: 'resources/views/hotdeals.html'
+        }).when('/login', {
+        	templateUrl: 'resources/views/login.html',
+        	controller : 'loginCtrl'
+        }).when('/search', {
+            templateUrl: 'hotdeals.html'
+        }).when('/customer/profile',{
+        templateUrl:'resources/views/customer/profile.html'
+        })
+        .when('/customer/payments',{
+        templateUrl:'resources/views/customer/payments.html',
+        controller:'paymentCtrl'
+        })
+        .when('/customer/orders',{
+        templateUrl:'resources/views/customer/orders.html',
+        controller:'orderCtrl'
+        })
+        .when('/vendor/register', {
+            templateUrl: 'resources/views/vendor-registration.html',
+            controller: 'regCtrl'
+        }).when('/products/:name', {
+            templateUrl: 'resources/views/searchProducts.html',
+            controller: 'productsCtrl'
+        }).when('/productDetail/:id', {
+            templateUrl: 'resources/views/productDetails.html',
+            controller: 'productDetailCtrl'
+    })
+        .otherwise({
+            templateUrl: '../views/hotdeals.html'
+        });
+}]);
+
+var rapp = angular.module('rapp', []);
