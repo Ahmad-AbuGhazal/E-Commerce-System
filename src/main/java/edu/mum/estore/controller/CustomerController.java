770c196db64cd499f74530725e50840006a6dce8
happybubblefish <congls2007@yahoo.com>
2016-10-16 16:29:15 -0400

    Reformat code

--- src/main/java/edu/mum/estore/controller/CustomerController.java
+++ src/main/java/edu/mum/estore/controller/CustomerController.java
@@ -5,12 +5,10 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.method.P;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,64 +24,67 @@ import edu.mum.estore.service.PaymentCardService;
 
 @RestController
 public class CustomerController {
-@Autowired 
-CustomerService customerService;
-@Autowired
-PaymentCardService paymentCardService;
-@Autowired
-OrderService orderService;
+	@Autowired
+	CustomerService customerService;
+	@Autowired
+	PaymentCardService paymentCardService;
+	@Autowired
+	OrderService orderService;
 
-@ResponseBody
-@RequestMapping(value="/customers/{id}")
-public Customer getProfile(@PathVariable("id") long id){
-	if(id<=0)
-	return null;
-	else 
-		return customerService.get(id);
-}
-
-
-@RequestMapping(value="/customers/{id}/payments",method=RequestMethod.POST)
-public ResponseInfo addPayment(@PathVariable("id") long id,@Valid@RequestBody PaymentCard card){
-	ResponseInfo info=new ResponseInfo();
-	Customer customer=customerService.get(id);
-	card.setCustomer(customer);
-	paymentCardService.save(card);
-	info.setResponse('Y');
-	return info;
-}
-
-@ResponseBody
-@RequestMapping(value="/customers/{id}/payments/{paymentId}",method=RequestMethod.GET)
-public void removePayment(@PathVariable("id") long customerId,@PathVariable("paymentId") long paymentId){
-	try
-	{
-	paymentCardService.remove(paymentId);
+	@ResponseBody
+	@RequestMapping(value = "/customers/{id}")
+	public Customer getProfile(@PathVariable("id") long id) {
+		if (id <= 0)
+			return null;
+		else
+			return customerService.get(id);
 	}
-	catch(Exception ex){
-	throw new PaymentNotValidException("Payment Card Not Found");
+
+	@RequestMapping(value = "/customers/{id}/payments", method = RequestMethod.POST)
+	public ResponseInfo addPayment(@PathVariable("id") long id, @Valid @RequestBody PaymentCard card) {
+		ResponseInfo info = new ResponseInfo();
+		Customer customer = customerService.get(id);
+		card.setCustomer(customer);
+		paymentCardService.save(card);
+		info.setResponse('Y');
+		return info;
 	}
-}
-@ResponseBody
-@RequestMapping(value="/customers/{id}/payments",method=RequestMethod.GET, produces="application/json")
-public  List<PaymentCard> getPayments(@PathVariable("id") long customerId){
-	return paymentCardService.getAll(customerId);
-}
-@ResponseBody
-@RequestMapping(value="/customers/{id}/payments/{paymentId}",method=RequestMethod.GET, produces="application/json")
-public  PaymentCard getPayment(@PathVariable("id") long customerId,@PathVariable("paymentId") long paymentId){
-	if(customerId>=0)
-	return paymentCardService.get(paymentId);
-	else return null;
-}
-@ResponseBody
-@RequestMapping(value="/customers/{id}/orders",method=RequestMethod.GET, produces="application/json")
-public  List<Order> getOrders(@PathVariable("id") long customerId){
-	return orderService.getAll(customerId);
-}
-@ResponseBody
-@RequestMapping(value="/customers/{id}/orders/{orderId}",method=RequestMethod.GET, produces="application/json")
-public  List<OrderDetails> getOrderDetails(@PathVariable("id") long customerId,@PathVariable("orderId") long orderId){
-	return orderService.getOrderDetails(orderId);
-}
+
+	@ResponseBody
+	@RequestMapping(value = "/customers/{id}/payments/{paymentId}", method = RequestMethod.GET)
+	public void removePayment(@PathVariable("id") long customerId, @PathVariable("paymentId") long paymentId) {
+		try {
+			paymentCardService.remove(paymentId);
+		} catch (Exception ex) {
+			throw new PaymentNotValidException("Payment Card Not Found");
+		}
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/customers/{id}/payments", method = RequestMethod.GET, produces = "application/json")
+	public List<PaymentCard> getPayments(@PathVariable("id") long customerId) {
+		return paymentCardService.getAll(customerId);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/customers/{id}/payments/{paymentId}", method = RequestMethod.GET, produces = "application/json")
+	public PaymentCard getPayment(@PathVariable("id") long customerId, @PathVariable("paymentId") long paymentId) {
+		if (customerId >= 0)
+			return paymentCardService.get(paymentId);
+		else
+			return null;
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/customers/{id}/orders", method = RequestMethod.GET, produces = "application/json")
+	public List<Order> getOrders(@PathVariable("id") long customerId) {
+		return orderService.getAll(customerId);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/customers/{id}/orders/{orderId}", method = RequestMethod.GET, produces = "application/json")
+	public List<OrderDetails> getOrderDetails(@PathVariable("id") long customerId,
+			@PathVariable("orderId") long orderId) {
+		return orderService.getOrderDetails(orderId);
+	}
 }
